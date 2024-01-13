a74f72717dd3804f26d9b1cb565327a94002c57b
mutanim24 <mutanim24@gmail.com>
2024-01-13 19:51:30 +0600

    added new font

--- src/page/Home/DollMartMagic/DollMartMagic.jsx
+++ src/page/Home/DollMartMagic/DollMartMagic.jsx
@@ -5,9 +5,9 @@ const DollMartMagic = () => {
     return (
         <div className='flex flex-row items-center bg-pink-700 py-7'>
             <div className='w-6/12 text-white m-8'>
-                <h4>Where Every Doll Finds a Home, and Every Smile Has a Story</h4>
+                <h4>Where Every Doll Finds a Home</h4>
                 <h1 className='text-5xl font-bold my mt-2 mb-4'>Doll Mart Magic</h1>
-                <p>At Doll Mart, the magic is in every moment and every doll. We're not just an online store; we're your enchanting destination for all things dolls. What sets Doll Mart apart? It's the curated collections that spark imagination, the quality that stands the test of playtime, and the joy that arrives with every package. We're more than a marketplace; we're your trusted source for doll dreams. Join us at Doll Mart, where every doll tells a story, and every purchase becomes a treasured memory. Experience the Doll Mart magic – because dolls are not just toys; they're lifelong companions</p>
+                <p>At Doll Mart, the magic is in every moment and every doll. We're not just an online store; we're your enchanting destination for all things dolls. What sets Doll Mart apart? It's the curated collections that spark imagination, the quality that stands the test of playtime, and the joy that arrives with every package.</p>
 
             </div>
             <div className='w-6/12'>
