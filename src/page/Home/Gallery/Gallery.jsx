3d4f47dd183d77ef11beb262bd2efd89e8d92bb7
mutanim24 <mutanim24@gmail.com>
2024-01-16 00:15:35 +0600

    updated misnary gallery

--- src/page/Home/Gallery/Gallery.jsx
+++ src/page/Home/Gallery/Gallery.jsx
@@ -11,38 +11,29 @@ import doll4 from '../../../assets/doll-4.jpg';
 import doll5 from '../../../assets/doll-5.jpg';
 import doll6 from '../../../assets/doll-6.jpg';
 import doll7 from '../../../assets/doll-7.jpg';
+import doll8 from '../../../assets/doll-8.jpg';
+import doll9 from '../../../assets/doll-9.jpg';
+import doll10 from '../../../assets/doll-10.jpg';
+import doll11 from '../../../assets/doll-11.jpg';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import PhotoAlbum from "react-photo-album";
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 
 const Gallery = () => {
-    const images = [doll1, doll2, doll3, doll4, doll5, doll6, doll7, fashion];
+    const images = [doll10, doll11, doll9, doll7, fashion, doll1, doll8];
     return (
         <div className='md:px-14 my-16'>
             <SectionTitle
                 title="Dolls Gallery"
                 subtitle="A Captivating Showcase of Exquisite Dolls"
             ></SectionTitle>
-            {/* <div>
-                <PhotoAlbum photos={doll1} layout="masonry" />
-                <PhotoAlbum photos={doll2} layout="masonry" />
-                <PhotoAlbum photos={doll3} layout="masonry" />
-                <PhotoAlbum photos={doll4} layout="masonry" />
-            </div> */}
 
-            <ResponsiveMasonry>
-                <Masonry gutter="20px">
-                    {
-                        images.map((item, index) => <div>
-                            <img src={item} alt="" />
-                        </div>)
-                    }
-                </Masonry>
-            </ResponsiveMasonry>
 
-            <div data-aos="zoom-in" className='grid grid-cols-2 md:grid-cols-4 p-4 md:p-8 border-dotted rounded border-pink-500 border-8'>
-                <div className="shadow-xl">
+
+
+            <div data-aos="zoom-in" className='p-4 md:p-8 border-dotted rounded border-pink-500 border-8'>
+                {/* <div className="shadow-xl">
                     <figure><img className='hover:animate-ping border-pink-600' src={doll1} alt="dolls" /></figure>
                 </div>
                 <div className="shadow-xl">
@@ -65,7 +56,17 @@ const Gallery = () => {
                 </div>
                 <div className="shadow-xl">
                     <figure><img className='hover:animate-ping border-pink-600' src={fashion} alt="dolls" /></figure>
-                </div>
+                </div> */}
+
+                <ResponsiveMasonry>
+                    <Masonry gutter="10px">
+                        {
+                            images.map((item, index) => <div>
+                                <img src={item} alt="" />
+                            </div>)
+                        }
+                    </Masonry>
+                </ResponsiveMasonry>
 
             </div>
         </div>
